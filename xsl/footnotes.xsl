transpect/docx2hub

commit 8fe4b511b9954fa8a6ea01f787f6a5f8869eae2c

decrease debug file size by avoiding repetitive namespace declarations

xsl/footnotes.xsl
@@ -20,7 +20,7 @@
   xmlns:docx2hub ="http://transpect.io/docx2hub"
   xmlns:tr="http://transpect.io"
   xmlns="http://docbook.org/ns/docbook"
-  exclude-result-prefixes = "w o v wx xs dbk pkg r rel word200x exsl saxon fn mml docx2hub tr"
+  exclude-result-prefixes = "fn xs w word200x v dbk wx o pkg r rel exsl saxon mml css docx2hub tr"
   >
 
   <!-- We don’t need to include MS Word localized style names if we trust it to
@@ -82,11 +82,18 @@
     <xsl:for-each-group select="* except w:rPr" group-starting-with="*[self::w:tab]">
       <xsl:sequence select="current-group()[self::w:tab]"/>
       <xsl:for-each select="$r">
-        <xsl:copy copy-namespaces="no">
+        <xsl:copy>
           <xsl:sequence select="@*, w:rPr, current-group()[not(self::w:tab)]"/>
         </xsl:copy>
       </xsl:for-each>
     </xsl:for-each-group>
+  </xsl:template>
+
+  <xsl:template match="*" mode="docx2hub:join-instrText-runs docx2hub:join-instrText-runs_footnote-tabs" priority="-0.25">
+    <!-- suppress excessive namespace declarations on every element -->
+    <xsl:copy>
+      <xsl:apply-templates select="@*, node()" mode="#current"/>
+    </xsl:copy>
   </xsl:template>
 
   <!-- The normal case is a run with rStyle='FootnoteReference' (or al localized name) with an element w:footnoteRef,

xsl/insert-xpath.xsl
@@ -25,7 +25,7 @@
   xmlns:ct="http://schemas.openxmlformats.org/package/2006/content-types"
   xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties"
   xmlns:docx2hub="http://transpect.io/docx2hub"
-  exclude-result-prefixes="xs docx2hub mml tr dbk cp"
+  exclude-result-prefixes="docx2hub mml tr dbk cp"
   version="2.0">
   
   <xsl:param name="srcpaths-on-runs" as="xs:string" select="'yes'"/>
@@ -81,13 +81,19 @@
                         [ not(ancestor::w:pPr) (: suppress srcpath in tab declarations :)] 
                         [ /*/name() = ('w:document', 'w:footnotes', 'w:endnotes', 'w:comments')]
                         " mode="insert-xpath">
-    <xsl:copy copy-namespaces="no">
+    <xsl:copy>
       <xsl:attribute name="srcpath" select="docx2hub:srcpath(.)"/>
       <xsl:apply-templates select="@* except @srcpath" mode="#current"/><!-- there was a case where the input was overwritten with an output that contained srcpaths -->
       <xsl:apply-templates mode="#current"/>
     </xsl:copy>
   </xsl:template>
 
+  <xsl:template match="*" mode="insert-xpath" priority="-0.25">
+    <!-- suppress excessive namespace declarations on every element -->
+    <xsl:copy>
+      <xsl:apply-templates select="@*, node()" mode="#current"/>
+    </xsl:copy>
+  </xsl:template>
   <xsl:template match="/w:document" mode="insert-xpath" as="document-node(element(w:root))" priority="2">
     <xsl:variable name="container-base-uri" select="replace($base-dir, 'word/$', '')" as="xs:string"/>
     <xsl:variable name="docRels-uri" as="xs:anyURI"
@@ -210,7 +216,7 @@
   <xsl:template match="  w:document | w:numbering | w:endnotes | w:footnotes 
                        | w:settings | w:fonts | rel:Relationships | w:comments 
                        | ct:Types | w:hdr | w:ftr | *:Properties | cp:coreProperties" mode="insert-xpath">
-    <xsl:copy copy-namespaces="no">
+    <xsl:copy>
       <xsl:attribute name="xml:base" select="base-uri()" />
       <xsl:apply-templates select="@*, *" mode="#current"/>      
     </xsl:copy>
@@ -240,7 +246,7 @@
 
   <xsl:template match="/w:styles" mode="insert-xpath">
     <xsl:param name="themes" as="document-node(element(a:theme))*" tunnel="yes"/>
-    <xsl:copy copy-namespaces="no">
+    <xsl:copy>
       <xsl:attribute name="xml:base" select="base-uri()" />
       <!-- Font des Standardtextes -->
       <xsl:variable name="normal" as="element(w:style)?"
@@ -284,7 +290,7 @@
     <xsl:param name="default-font" as="xs:string?" tunnel="yes"/>
     <xsl:param name="default-font-size" as="xs:string" tunnel="yes"/>
     <xsl:param name="default-lang" as="xs:string?" tunnel="yes"/>
-    <xsl:copy copy-namespaces="no">
+    <xsl:copy>
       <xsl:apply-templates select="@*, *" mode="#current"/>
       <xsl:if test="not(w:sz) and $default-font-size">
         <w:sz w:val="{$default-font-size}"/>
@@ -301,7 +307,7 @@
   <xsl:template match="w:style[@w:type = 'paragraph']
                               [not(w:basedOn)]/w:rPr/w:lang[not(@w:val)]" mode="insert-xpath">
     <xsl:param name="default-lang" as="xs:string?" tunnel="yes"/>
-    <xsl:copy copy-namespaces="no">
+    <xsl:copy>
       <xsl:attribute name="w:val" select="$default-lang"/>
       <xsl:sequence select="@*"/>
     </xsl:copy>
@@ -313,7 +319,7 @@
     <xsl:param name="default-font" as="xs:string?" tunnel="yes"/>
     <xsl:param name="default-font-size" as="xs:string" tunnel="yes"/>
     <xsl:param name="default-lang" as="xs:string?" tunnel="yes"/>
-    <xsl:copy copy-namespaces="no">
+    <xsl:copy>
       <xsl:apply-templates select="@*, node()" mode="#current"/>
       <w:rPr>
         <xsl:if test="$default-font-size">

xsl/join-runs.xsl
@@ -502,7 +502,7 @@
   
   <!-- This mode has to run before docx2hub:field-functions --> 
   <xsl:template match="*[w:r/w:instrText]" mode="docx2hub:join-instrText-runs">
-    <xsl:copy copy-namespaces="no">
+    <xsl:copy>
       <xsl:apply-templates select="@*" mode="#current"/>
       <xsl:for-each-group select="node()" 
         group-adjacent="exists(self::w:r[w:instrText][every $c in * satisfies $c/(self::w:instrText | self::w:br (: w:br appeared in comments in 12181_2015_0024_Manuscript.docm :))]

xsl/modules/prop-mapping/map-props.xsl
@@ -22,7 +22,7 @@
   xmlns:customProps="http://schemas.openxmlformats.org/officeDocument/2006/custom-properties"
   xmlns="http://docbook.org/ns/docbook"
   version="2.0"
-  exclude-result-prefixes="#all">
+  exclude-result-prefixes="w xs dbk r rel tr m docx2hub v wp">
 
   <xsl:import href="propmap.xsl"/>
   <xsl:import href="http://transpect.io/xslt-util/colors/xsl/colors.xsl"/>
@@ -253,6 +253,14 @@
 
   <xsl:template match="w:basedOn/@w:val" mode="docx2hub:add-props" />
 
+  <xsl:template match="*" mode="docx2hub:join-instrText-runs docx2hub:remove-redundant-run-atts 
+    docx2hub:add-props docx2hub:XML-Hubformat-add-properties_layout-type" priority="-0.25">
+    <!-- suppress excessive namespace declarations on every element -->
+    <xsl:copy>
+      <xsl:apply-templates select="@*, node()" mode="#current"/>
+    </xsl:copy>
+  </xsl:template>
+
   <xsl:template match="w:style"
     mode="docx2hub:XML-Hubformat-add-properties_layout-type">
     <xsl:param name="version" tunnel="yes" as="xs:string"/>
@@ -325,7 +333,7 @@
   
   <xsl:template match="w:style/w:pPr/w:numPr[not(w:ilvl)]" mode="docx2hub:add-props" priority="1">
     <xsl:param name="ilvl" as="xs:integer?" tunnel="yes"/>
-    <xsl:copy copy-namespaces="no">
+    <xsl:copy>
       <xsl:apply-templates select="*" mode="#current"/>
       <xsl:if test="$ilvl">
         <w:ilvl w:val="{$ilvl}"/>  
@@ -334,7 +342,7 @@
   </xsl:template>
   
   <xsl:template match="w:lvl/w:rPr | w:lvl/w:pPr" mode="docx2hub:add-props" priority="3">
-    <xsl:copy copy-namespaces="no">
+    <xsl:copy>
       <xsl:apply-templates select="*" mode="#current" />
       <!-- in order for subsequent (numbering.xsl) symbol mappings, the original rFonts must also be preserved -->
       <xsl:sequence select="*"/>
@@ -345,7 +353,7 @@
   <xsl:template match="w:pPr/w:rPr" mode="docx2hub:add-props" priority="2.5" />
 
   <xsl:template match="w:tblPr | w:tblPrEx" mode="docx2hub:add-props" priority="2">
-    <xsl:copy copy-namespaces="no">
+    <xsl:copy>
       <xsl:apply-templates select="*" mode="#current" />
     </xsl:copy>
   </xsl:template>
@@ -1261,7 +1269,7 @@
   <!-- collateral: denote numbering resets -->
   <xsl:template match="w:p" mode="docx2hub:remove-redundant-run-atts">
     <xsl:param name="css:orientation" as="xs:string?" tunnel="yes"/>
-    <xsl:copy copy-namespaces="no">
+    <xsl:copy>
       <xsl:variable name="style" select="key('docx2hub:style-by-role', @role, $root)[1]" as="element(css:rule)?"/>
       <xsl:variable name="numId" as="element(w:numId)?" 
         select="(w:numPr/w:numId, $style/w:numPr/w:numId)[1]"/>

xsl/objects.xsl
@@ -56,7 +56,7 @@
   <xsl:template match="mc:AlternateContent/mc:Choice[$docx2hub:discard-alternate-choices]" mode="docx2hub:add-props"/>
 
   <xsl:template match="@* | * | w:drawing | w:txbxContent | w:pict" mode="foreign">
-    <xsl:copy copy-namespaces="no">
+    <xsl:copy >
       <xsl:apply-templates select="@*, node()" mode="#current"/>
     </xsl:copy>
   </xsl:template>

xsl/wml2dbk.xsl
@@ -104,6 +104,13 @@
 
   <!-- Each field function will be replaced with an XML element with the same name as the field function -->
 
+  <xsl:template match="*" mode="docx2hub:field-functions" priority="-0.25">
+    <!-- suppress excessive namespace declarations on every element -->
+    <xsl:copy>
+      <xsl:apply-templates select="@*, node()" mode="#current"/>
+    </xsl:copy>
+  </xsl:template>
+  
   <xsl:template match="/" mode="docx2hub:field-functions">
     <xsl:variable name="field-begins" as="element(w:fldChar)*" 
       select="for $it in .//w:instrText
@@ -498,7 +505,7 @@
   <!-- collateral, has to happen before wml-to-hub because redundant xml:lang attributes will
     be eliminated then (redundant = same as this top-level language) -->
   <xsl:template match="/dbk:*" mode="docx2hub:join-instrText-runs">
-    <xsl:copy copy-namespaces="no">
+    <xsl:copy>
       <xsl:apply-templates select="@*" mode="#current"/>
       <xsl:variable name="most-frequent-lang" select="docx2hub:most-frequent-lang(.)" as="xs:string?"/>
       <xsl:if test="exists($most-frequent-lang)">

commit f777cbde0993a28e0da9ddf19363993721f102d7

fix fn mark processing error introduced in 8fe4b51 / r401

xsl/footnotes.xsl
@@ -89,7 +89,7 @@
     </xsl:for-each-group>
   </xsl:template>
 
-  <xsl:template match="*" mode="docx2hub:join-instrText-runs docx2hub:join-instrText-runs_footnote-tabs" priority="-0.25">
+  <xsl:template match="*" mode="docx2hub:join-instrText-runs" priority="-0.25">
     <!-- suppress excessive namespace declarations on every element -->
     <xsl:copy>
       <xsl:apply-templates select="@*, node()" mode="#current"/>